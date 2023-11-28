969e829521a800a82a85955484c2d3a96502f6e9
BuiNhat2607 <nhatminhbui2607@gmail.com>
2023-11-28 13:35:54 +0700

    pull code dev moi

--- src/main/java/com/example/dacn/modules/category/service/CategoryService.java
+++ src/main/java/com/example/dacn/modules/category/service/CategoryService.java
@@ -44,7 +44,7 @@ public class CategoryService
         }
     }
 
-    public ResponseModel createCategory(MultipartFile file, CategoryDTO categoryDTO)
+    public ResponseModel createCategory( CategoryDTO categoryDTO)
     {
         categoryDTO.setCategoryID("");
 
@@ -55,7 +55,7 @@ public class CategoryService
 
             categoryRepository.save(newCategory);
             String categoryId=categoryRepository.findLatestAddressId();
-            String imageUrl=imageService.uploadImage(file,"categoryImage/",categoryId);
+            String imageUrl=imageService.uploadImage(categoryDTO.getImage(), "categoryImage/",categoryId);
             newCategory.setCategoryID(categoryId);
             newCategory.setImageUrl(imageUrl);
             Category savedCate=categoryRepository.save(newCategory);
